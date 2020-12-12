7517bcdbcdf84b47b511118bbbe5cc875e058cee
Kalyani Goyal <kalyani.goyal@eagleview.com>
2020-12-12 12:42:51 +0530

    Part4: Improving Front end, pkg backend

---
+++ front-end/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./Header.module.css";
+
+const Header = () => (
+    <div className = {styles.header}>
+        <h2> Realtime Chat App </h2>
+    </div>
+);
+
+export default Header;
